65a7e89f3bf2ef3f5d6392191a29c093d1388fbd
Florin Olteanu <olteanuflorin86@gmail.com>
2023-06-29 09:34:07 +0300

    RadioButtons

---
+++ src/application/Controller.java
@@ -0,0 +1,26 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+public class Controller {
+
+	@FXML
+	private Label myLabel;
+	@FXML
+	private RadioButton rButton1, rButton2, rButton3;
+	
+	public void getFood(ActionEvent event) {
+		if(rButton1.isSelected()) {
+			myLabel.setText(rButton1.getText());
+		} else if(rButton2.isSelected()) {
+			myLabel.setText(rButton2.getText());
+		} else if(rButton3.isSelected()) {
+			myLabel.setText(rButton3.getText());
+		} else {
+			myLabel.setText("Select what you want to eat");
+		}
+	}
+}
