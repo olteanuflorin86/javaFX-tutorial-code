9ff38c47a21f5f84ad3197737aba9fd6b7f4e69f
Florin Olteanu <olteanuflorin86@gmail.com>
2023-06-29 15:49:11 +0300

    MenuBar

---
+++ src/application/Controller.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Controller implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void newMethod() {
+		System.out.println("New Menu Item");
+	}
+
+}
